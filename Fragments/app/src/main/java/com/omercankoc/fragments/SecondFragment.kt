5ce6c6a0df3db42e19e2e8e6b4166d5c83d467c5
Ömer Can Koç <63169919+omercankoc@users.noreply.github.com>
2021-05-11 13:36:00 +0300

    - Added second fragment

--- Fragments/app/src/main/java/com/omercankoc/fragments/SecondFragment.kt
+++ Fragments/app/src/main/java/com/omercankoc/fragments/SecondFragment.kt
@@ -6,11 +6,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-class FirstFragment : Fragment() {
+class SecondFragment : Fragment() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
     override fun onCreateView(
@@ -18,6 +18,6 @@ class FirstFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        return inflater.inflate(R.layout.fragment_second, container, false)
     }
 }
